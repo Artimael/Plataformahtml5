6d5129f0d509922af51e6e9420150807cf6cdeab
Artimael <weichongl@hotmail.com>
2016-01-20 17:52:59 -0300

    Clase 3 Preparacion

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var stage;
+
+stage= new Kinetic.Stage({
+// Crea stage ( canvas automaticamente)Estoy usando un Framework
+    container: 'game', // contenedor div donde se mostrara
+    width:960,
+    height:500
+}); 
